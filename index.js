e93adc02963e5d17348d35cc2ab7f014067cb2c7
Alexey Kostin <alexey-kostin@yandex.ru>
2022-06-09 18:16:14 +0300

    fix: прописать типы кнопок и внести улучшения по ревью

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
+let form = document.querySelector('.popup__form');
+
+let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
 let saveButton = document.querySelector('.popup__form-submit-button');
 
@@ -9,32 +11,37 @@ let pageStatus = document.querySelector('.profile__status');
 let fieldName = document.querySelector('.popup__form-field_data_name');
 let fieldStatus = document.querySelector('.popup__form-field_data_status');
 
-let form = document.querySelector('.popup__form');
-
-function submitForm(event) {
-  event.preventDefault();
+function fillFields() {
+  fieldName.value = pageName.textContent;
+  fieldStatus.value = pageStatus.textContent;
 }
 
-form.addEventListener('submit', submitForm);
+function saveFields() {
+  pageName.textContent = fieldName.value;
+  pageStatus.textContent = fieldStatus.value;
+}
 
 function openPopup() {
   popup.classList.remove('popup_closed');
-  fieldName.value = pageName.textContent;
-  fieldStatus.value = pageStatus.textContent;
+  fillFields();
 }
 
 function closePopup() {
   popup.classList.add('popup_closed');
 }
 
-function savePopup() {
+function saveForm() {
   if (fieldName.value && fieldStatus.value) {
-    pageName.textContent = fieldName.value;
-    pageStatus.textContent = fieldStatus.value;
-    popup.classList.add('popup_closed');
+    saveFields();
+    closePopup();
   }
 }
 
-editButton.addEventListener('click',openPopup);
-closeButton.addEventListener('click',closePopup);
-saveButton.addEventListener('click',savePopup);
+function disableDefaultSubmit(event) {
+  event.preventDefault();
+}
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+saveButton.addEventListener('click', saveForm);
+form.addEventListener('submit', disableDefaultSubmit);
