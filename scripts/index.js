715fff3067820e669531c5c8bffef00d11269c46
Alexey Kostin <alexey-kostin@yandex.ru>
2022-06-24 17:47:01 +0300

    feat: Создавать карточки первоначально из js массива

--- scripts/index.js
+++ scripts/index.js
@@ -45,3 +45,49 @@ editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 saveButton.addEventListener('click', saveForm);
 form.addEventListener('submit', disableDefaultSubmit);
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
+const cardContainer = document.querySelector('.gallery');
+
+function buildCards() {
+  const templateCard = document.querySelector('#card').content;
+  for (let i = 0; i < initialCards.length; i++) {
+    const currentCard = templateCard.cloneNode(true);
+    const cardImg = currentCard.querySelector('.gallery__photo');
+    cardImg.src = initialCards[i].link;
+    const cardTitle = currentCard.querySelector('.gallery__title');
+    cardTitle.textContent = initialCards[i].name;
+    cardImg.alt = initialCards[i].name;
+    cardContainer.append(currentCard);
+  }
+}
+
+buildCards();
