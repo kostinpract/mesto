853f59721d86ad6d92f296da591c31ce943cdba8
Alexey Kostin <alexey-kostin@yandex.ru>
2022-06-30 16:08:48 +0300

    hotfix: переделать как сказал ревьювер

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,47 @@
 const userEditButton = document.querySelector('.profile__edit-button');
 const userPopup = document.querySelector('.popup_userinfo');
-const userPopupCloseButton = document.querySelector('.popup__close-button_data_userinfo');
+const userPopupCloseButton = userPopup.querySelector('.popup__close-button_data_userinfo');
 const userNameText = document.querySelector('.profile__name');
 const userStatusText = document.querySelector('.profile__status');
 
 const userForm = document.querySelector('.popup__form_data_userinfo');
-const userNameField = document.querySelector('.popup__form-field_data_name');
-const userStatusField = document.querySelector('.popup__form-field_data_status');
-const userSaveButton = document.querySelector('.popup__form-submit-button_data_userinfo');
+const userNameField = userForm.querySelector('.popup__form-field_data_name');
+const userStatusField = userForm.querySelector('.popup__form-field_data_status');
+const userSaveButton = userForm.querySelector('.popup__form-submit-button_data_userinfo');
 
 const cardAddButton = document.querySelector('.profile__add-button');
 const cardAddPopup = document.querySelector('.popup_addcard');
-const cardAddPopupCloseButton = document.querySelector('.popup__close-button_data_addcard');
+const cardAddPopupCloseButton = cardAddPopup.querySelector('.popup__close-button_data_addcard');
 
 const cardAddForm = document.querySelector('.popup__form_data_addcard');
-const cardAddTitleField = document.querySelector('.popup__form-field_data_card-title');
-const cardAddImageField = document.querySelector('.popup__form-field_data_card-image');
-const cardAddSaveButton = document.querySelector('.popup__form-submit-button_data_addcard');
-
-const cardRemoveButton = document.querySelector('.gallery__remove-button');
-
-const photoPopup = document.querySelector('.popup_photo');
-const photoPopupCloseButton = document.querySelector('.popup__close-button_data_big-image');
+const cardAddTitleField = cardAddForm.querySelector('.popup__form-field_data_card-title');
+const cardAddImageField = cardAddForm.querySelector('.popup__form-field_data_card-image');
+const cardAddSaveButton = cardAddForm.querySelector('.popup__form-submit-button_data_addcard');
 
 const cardContainer = document.querySelector('.gallery');
+const cardRemoveButton = cardContainer.querySelector('.gallery__remove-button');
+
+const photoPopup = document.querySelector('.popup_photo');
+const bigImg = photoPopup.querySelector('.popup__big-image-photo');
+const bigImgTitle = photoPopup.querySelector('.popup__big-image-title');
+const photoPopupCloseButton = photoPopup.querySelector('.popup__close-button_data_big-image');
 
 initialCards.forEach( (item) => cardContainer.append( createNewCard(item.link, item.name) ) );
 
-userEditButton.addEventListener( 'click', (evt) => {
+userEditButton.addEventListener( 'click', () => {
   fillUserFields();
   openPopup(userPopup);
 });
-userPopupCloseButton.addEventListener( 'click', (evt) => closePopup(userPopup) );
+userPopupCloseButton.addEventListener( 'click', () => closePopup(userPopup) );
 userSaveButton.addEventListener( 'click', saveUserForm );
 userForm.addEventListener( 'submit', disableDefaultSubmit );
 
-cardAddButton.addEventListener( 'click', (evt) => openPopup(cardAddPopup) );
-cardAddPopupCloseButton.addEventListener( 'click', (evt) => closePopup(cardAddPopup) );
+cardAddButton.addEventListener( 'click', () => openPopup(cardAddPopup) );
+cardAddPopupCloseButton.addEventListener( 'click', () => closePopup(cardAddPopup) );
 cardAddSaveButton.addEventListener( 'click', saveCardForm );
 cardAddForm.addEventListener( 'submit', disableDefaultSubmit );
 
-photoPopupCloseButton.addEventListener( 'click', (evt) => closePopup(photoPopup) );
+photoPopupCloseButton.addEventListener( 'click', () => closePopup(photoPopup) );
 
 function openPopup(popup) {
   popup.classList.add('popup_shown');
@@ -71,35 +72,28 @@ function saveUserForm() {
   }
 }
 
-function removeCard(evt) {
-  const wholeCard = evt.target.closest('li');
-  wholeCard.remove();
+function likeCard(card) {
+  card.classList.toggle('gallery__like-button_active');
 }
 
-function likeCard(evt) {
-  evt.target.classList.toggle('gallery__like-button_active');
+function removeCard(card) {
+  card.remove();
 }
 
-function fillPhotoPopup(evt) {
-  const bigImg = photoPopup.querySelector('.popup__big-image-photo');
-  const bigImgTitle = photoPopup.querySelector('.popup__big-image-title');
-
-  const thisImg = evt.target;
-  const thisImgTitle = evt.target.closest('li').querySelector('.gallery__title');
-
-  bigImg.src = thisImg.src;
-  bigImg.alt = thisImg.alt;
-  bigImgTitle.textContent = thisImgTitle.textContent;
+function fillPhotoPopup(image, title) {
+  bigImg.src = image;
+  bigImg.alt = title;
+  bigImgTitle.textContent = title;
 }
 
-function showPhotoPopup(evt) {
-  fillPhotoPopup(evt);
+function showPhotoPopup(image, title) {
+  fillPhotoPopup(image, title);
   openPopup(photoPopup);
 }
 
 function createNewCard(image, title) {
   const templateCard = document.querySelector('#card').content;
-  const currentCard = templateCard.cloneNode(true);
+  const currentCard = templateCard.querySelector('.gallery__item').cloneNode(true);
 
   const cardImg = currentCard.querySelector('.gallery__photo');
   cardImg.src = image;
@@ -108,9 +102,9 @@ function createNewCard(image, title) {
   cardTitle.textContent = title;
   cardImg.alt = title;
 
-  cardImg.addEventListener( 'click', (evt) => showPhotoPopup(evt) );
-  currentCard.querySelector('.gallery__like-button').addEventListener( 'click', (evt) => likeCard(evt) );
-  currentCard.querySelector('.gallery__remove-button').addEventListener( 'click', (evt) => removeCard(evt) );
+  cardImg.addEventListener( 'click', () => showPhotoPopup(image, title) );
+  currentCard.querySelector('.gallery__like-button').addEventListener( 'click', () => likeCard(currentCard) );
+  currentCard.querySelector('.gallery__remove-button').addEventListener( 'click', () => removeCard(currentCard) );
 
   return currentCard;
 }
@@ -122,7 +116,8 @@ function clearCardForm() {
 
 function saveCardForm() {
   if (cardAddTitleField.value && (cardAddImageField.value.startsWith('http://') || cardAddImageField.value.startsWith('https://'))) {
-    cardContainer.prepend( createNewCard(cardAddImageField.value, cardAddTitleField.value) );
+    const cardNew = createNewCard(cardAddImageField.value, cardAddTitleField.value);
+    cardContainer.prepend( cardNew );
     closePopup(cardAddPopup);
     clearCardForm();
   }
