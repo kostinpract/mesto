7d93d78d9bf1c8c47e66c630804cb7c661480122
Alexey Kostin <alexey-kostin@yandex.ru>
2022-07-25 15:41:01 +0300

    Сделать рефакторинг с классами Card и FormValidator

---
+++ scripts/Card.js
@@ -0,0 +1,63 @@
+import { showPhotoPopup } from './index.js'
+
+export default class Card {
+
+  constructor(title, image, templateSelector) {
+    this._title = title;
+    this._image = image;
+    this._isLiked = false;
+    this._templateSelector = templateSelector;
+  }
+
+  _getElement() {
+  	const cardElement = document
+      .querySelector(this._templateSelector)
+      .content
+      .querySelector('.gallery__item')
+      .cloneNode(true);
+
+    return cardElement;
+  }
+
+  generate() {
+    this._element = this._getElement();
+
+    this._elementPhoto = this._element.querySelector('.gallery__photo');
+    this._elementTitle = this._element.querySelector('.gallery__title');
+    this._elementTrash = this._element.querySelector('.gallery__remove-button');
+    this._elementLike = this._element.querySelector('.gallery__like-button');
+
+    this._elementPhoto.src = this._image;
+    this._elementPhoto.alt = this._title;
+    this._elementTitle.textContent = this._title;
+
+    this._setEventListeners();
+
+  	return this._element;
+  }
+
+  _setEventListeners() {
+    this._elementPhoto.addEventListener('click', () => {
+			this._handlePhotoClick();
+		});
+    this._elementLike.addEventListener( 'click', () => {
+			this._handleLikeClick();
+		});
+    this._elementTrash.addEventListener( 'click', () => {
+			this._handleRemoveClick();
+		});
+  }
+
+  _handlePhotoClick() {
+    showPhotoPopup(this._image, this._title);
+  }
+
+  _handleLikeClick() {
+    this._elementLike.classList.toggle('gallery__like-button_active');
+  }
+
+  _handleRemoveClick() {
+    this._element.remove();
+  }
+
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,7 @@
+import Card from './Card.js';
+import FormValidator from './FormValidator.js';
+import { initialCards } from './data.js';
+
 const userEditButton = document.querySelector('.profile__edit-button');
 const userPopup = document.querySelector('.popup_userinfo');
 const userNameText = document.querySelector('.profile__name');
@@ -16,13 +20,25 @@ const cardAddTitleField = cardAddForm.querySelector('.popup__form-field_data_car
 const cardAddSubmitButon = document.querySelector('.popup__form-submit-button_data_addcard');
 
 const cardContainer = document.querySelector('.gallery');
-const cardRemoveButton = cardContainer.querySelector('.gallery__remove-button');
+const cardTemplateSelector = '#card';
 
 const photoPopup = document.querySelector('.popup_photo');
 const bigImg = photoPopup.querySelector('.popup__big-image-photo');
 const bigImgTitle = photoPopup.querySelector('.popup__big-image-title');
 
+const settings = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__form-field',
+  submitButtonSelector: '.popup__form-submit-button',
+  inactiveButtonClass: 'popup__form-submit-button_error',
+  inputErrorClass: 'popup__form-field_error',
+  messageErrorClass: 'popup__form-warning_active'
+}
+
+// все попапы кладём в массив
 const popups = Array.from(document.querySelectorAll('.popup'));
+
+// проходимся по попапам и навешиваем закрытие по клику на оверлей и кнопку
 popups.forEach((popup) => {
   popup.addEventListener('mousedown', (evt) => {
     if (evt.target.classList.contains('popup_shown')) {
@@ -34,104 +50,113 @@ popups.forEach((popup) => {
   });
 });
 
+// заполняем поля формы профиля
 fillUserFields();
 
-initialCards.forEach( (item) => cardContainer.append( createNewCard(item.link, item.name) ) );
+// создаём и рендерим карточку с помощью класса
+const createCard = (item) => {
+  const cardObj = new Card(item.name, item.link, cardTemplateSelector);
+  cardContainer.prepend( cardObj.generate() );
+}
 
+// разворачиваем массив, чтобы использовать универсальную функцию рендеринга с prepend
+initialCards.reverse();
+
+// создаём и рендерим карточки из исходного массива
+initialCards.forEach(createCard);
+
+// отменяем дефолтный сабмит формы
+const disableDefaultSubmit = (event) => {
+  event.preventDefault();
+}
+
+// вешаем обработчики на кнопку и форму редактирования профиля
 userEditButton.addEventListener( 'click', () => openPopup(userPopup) );
 userForm.addEventListener( 'submit', saveUserForm );
 
+// вешаем обработчики на кнопку и форму добавления карточки
 cardAddButton.addEventListener( 'click', () => openPopup(cardAddPopup) );
 cardAddForm.addEventListener( 'submit', saveCardForm );
 
+// в этой глобальной переменной будем хранить элемент открытого попапа
 let popupOpened;
 
+// отслеживаем нажатие Escape и закрываем открытый попап
 const handleEscapeKey = (evt) => {
   if (evt.key === 'Escape') {
     closePopup(popupOpened);
   }
 }
 
+// открываем попап и навешиваем обработчик на Escape
 function openPopup(popup) {
   popup.classList.add('popup_shown');
   popupOpened = popup;
   window.addEventListener('keydown', handleEscapeKey);
 }
 
+// закрываем попап и убираем обработчик с Escape
 function closePopup(popup) {
   popup.classList.remove('popup_shown');
   window.removeEventListener('keydown', handleEscapeKey);
   popupOpened = undefined;
 }
 
-const disableDefaultSubmit = (event) => {
-  event.preventDefault();
-}
-
+// заполняем поля в форме профиля данными со страницы
 function fillUserFields() {
   userNameField.value = userNameText.textContent;
   userStatusField.value = userStatusText.textContent;
 }
 
+// заполняем данные на странице значениями из полей формы профиля
 function saveUserFields() {
   userNameText.textContent = userNameField.value;
   userStatusText.textContent = userStatusField.value;
 }
 
+// сохраняем данные из формы и закрываем попап
 function saveUserForm(evt) {
   disableDefaultSubmit(evt);
   saveUserFields();
   closePopup(userPopup);
 }
 
-function likeCard(card) {
-  card.querySelector('.gallery__like-button').classList.toggle('gallery__like-button_active');
-}
-
-function removeCard(card) {
-  card.remove();
-}
-
+// заполняем фото-попап фоткой и подписью
 function fillPhotoPopup(image, title) {
   bigImg.src = image;
   bigImg.alt = title;
   bigImgTitle.textContent = title;
 }
 
-function showPhotoPopup(image, title) {
+// открываем фото-попап
+export function showPhotoPopup(image, title) {
   fillPhotoPopup(image, title);
   openPopup(photoPopup);
 }
 
-function createNewCard(image, title) {
-  const templateCard = document.querySelector('#card').content;
-  const currentCard = templateCard.querySelector('.gallery__item').cloneNode(true);
-
-  const cardImg = currentCard.querySelector('.gallery__photo');
-  cardImg.src = image;
-
-  const cardTitle = currentCard.querySelector('.gallery__title');
-  cardTitle.textContent = title;
-  cardImg.alt = title;
-
-  cardImg.addEventListener( 'click', () => showPhotoPopup(image, title) );
-  currentCard.querySelector('.gallery__like-button').addEventListener( 'click', () => likeCard(currentCard) );
-  currentCard.querySelector('.gallery__remove-button').addEventListener( 'click', () => removeCard(currentCard) );
-
-  return currentCard;
-}
-
+// сбрасываем форму профиля и деактивируем кнопку сабмита
 function clearCardForm() {
   cardAddForm.reset();
-  // далее бы пригодилась функция toggleButtonActive, но validate.js подключается позже, поэтому "вручную"
-  cardAddSubmitButon.disabled = true;
-  cardAddSubmitButon.classList.add('popup__form-submit-button_error');
 }
 
+// создаём и рендерим новую карточку
+function addNewCard() {
+  createCard({name: cardAddTitleField.value, link: cardAddImageField.value});
+}
+
+// создаём карточку по данным из формы, закрываем попап формы, сбрасываем форму
 function saveCardForm(evt) {
   disableDefaultSubmit(evt);
-  const cardNew = createNewCard(cardAddImageField.value, cardAddTitleField.value);
-  cardContainer.prepend( cardNew );
+  addNewCard();
   closePopup(cardAddPopup);
   clearCardForm();
 }
+
+// все формы кладём в массив
+const formsAll = Array.from(document.querySelectorAll(settings.formSelector));
+
+// проходимся по массиву и создаём объект валидации для каждой формы, а также запускаем валидацию
+formsAll.forEach( (formElement) => {
+  const form = new FormValidator(settings, formElement);
+  form.enableValidation();
+});
