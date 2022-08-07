c093d3b50c885472995e6a255bc37f371bfb8bcb
Alexey Kostin <alexey-kostin@yandex.ru>
2022-08-07 18:07:27 +0300

    feature: add webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+import '../pages/index.css'; // добавьте импорт главного файла стилей 
+
 import Card from './Card.js';
 import FormValidator from './FormValidator.js';
 import { initialCards } from './data.js';
